3f13a068d43ecdad02b071ba153dde6e36b62b25
Line Le Goff <line.chloe.legoff@gmail.com>
2018-03-19 16:40:48 +0200

    boyscouting of NameBuilder

--- NameBuilder.py
+++ NameBuilder.py
@@ -4,9 +4,7 @@ from Names import Names
 
 class NameBuilder:
 	def __init__(self, root, group):
-		self.root = root
-		self.group = group
-		self.reference_dict = js.import_obj(self.root+'/'+self.group+'/ref_names.csv')
+		self.reference_dict = js.import_obj(root+'/'+group+'/ref_names.csv')
 
 	def build(self, name):
 		return Names(self.reference_dict[name])
