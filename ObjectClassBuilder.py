b95543f0970cec3607323a6ea092437ed24bc41a
Line Le Goff <line.chloe.legoff@gmail.com>
2018-03-21 17:10:38 +0200

    boyscout on ObjectClassBuilder

--- ObjectClassBuilder.py
+++ ObjectClassBuilder.py
@@ -5,6 +5,6 @@ class ObjectClassBuilder:
 	@staticmethod
 	def build(class_self, array, name):
 		class_self.name = name.name
+		class_self.array = array
 		for key, value in name.dict.items():
 			class_self.__dict__[key] = value
-			class_self.array = array
