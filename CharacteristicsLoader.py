35eb19a62abef004c24dd9f903b614da6ec8599b
Line Le Goff <line.chloe.legoff@gmail.com>
2018-03-21 16:30:14 +0200

    convert characteristics indexes as integers

--- CharacteristicsLoader.py
+++ CharacteristicsLoader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from Characteristics import Characteristics
 from JsonFiles import JsonFiles
@@ -11,25 +12,38 @@ class CharacteristicsLoader:
 	def build(self, name):
 		chara_dict = JsonFiles.import_obj(self.root+name.category+'/Characteristics.json')
 		if name.name in ['session', 'trial', 'n_frames', 'fps', 'mm2px', 'food_radius']:
-			return Characteristics(pd.DataFrame(
-				[[key, chara_dict[key][name.name]] for key in chara_dict.keys()],
-				columns=['id_exp', 'name']), name)
+			res = pd.DataFrame(
+				[chara_dict[key][name.name] for key in chara_dict.keys()],
+				index=np.array(list(chara_dict.keys()), dtype=float),
+				columns=[name.name])
+			res.index.name = 'id_exp'
 		elif name.name in ['food_center', 'traj_translation']:
-			return Characteristics(pd.DataFrame(
-				[[key]+chara_dict[key][name.name] for key in chara_dict.keys()],
-				columns=['id_exp', 'x', 'y']), name)
+			res = pd.DataFrame(
+				[chara_dict[key][name.name] for key in chara_dict.keys()],
+				index=np.array(list(chara_dict.keys()), dtype=float),
+				columns=['x', 'y'])
+			res.index.name = 'id_exp'
 		elif name.name in ['crop_limit_x', 'crop_limit_y']:
-			return Characteristics(pd.DataFrame(
-				[[key]+chara_dict[key][name.name] for key in chara_dict.keys()],
-				columns=['id_exp', 'l_min', 'l_max']), name)
+			res = pd.DataFrame(
+				[chara_dict[key][name.name] for key in chara_dict.keys()],
+				index=np.array(list(chara_dict.keys()), dtype=float),
+				columns=['l_min', 'l_max'])
+			res.index.name = 'id_exp'
 		elif name.name in ['ref_pts']:
-			return Characteristics(pd.DataFrame(
-				[[key]+chara_dict[key][name.name][0]+chara_dict[key][name.name][1] for key in chara_dict.keys()],
-				columns=['id_exp', 'x1', 'y1', 'x2', 'y2']), name)
+			res = pd.DataFrame(
+				[chara_dict[key][name.name][0]+chara_dict[key][name.name][1] for key in chara_dict.keys()],
+				index=np.array(list(chara_dict.keys()), dtype=float),
+				columns=['x1', 'y1', 'x2', 'y2'])
+			res.index.name = 'id_exp'
 		elif name.name in ['entrance']:
-			return Characteristics(pd.DataFrame(
-				[[key]+chara_dict[key][name.name][0]+chara_dict[key][name.name][1]+chara_dict[key][name.name][2]
+			res = pd.DataFrame(
+				[chara_dict[key][name.name][0]+chara_dict[key][name.name][1]+chara_dict[key][name.name][2]
 					for key in chara_dict.keys()],
-				columns=['id_exp', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3']), name)
+				index=np.array(list(chara_dict.keys()), dtype=int),
+				columns=['x1', 'y1', 'x2', 'y2', 'x3', 'y3'])
+			res.index.name = 'id_exp'
 		else:
 			print(name+' is not a characteristic')
+			res = None
+
+		return Characteristics(res, name)
--- script_test.py
+++ script_test.py
@@ -9,13 +9,15 @@ group = 'FMAB'
 # Chara = Characteristics(root, group)
 # print(Chara.get_array('entrance').head())
 
-#
-ExperimentBuilder = ExperimentGroupBuilder(root)
 
-FMAB = ExperimentBuilder.build(group)
-FMAB.load(['x0', 'y0', 'markings', 'food_radius', 'entrance'])
-print(FMAB.x0.description, FMAB.x0.name)
-print(FMAB.food_radius.description, FMAB.food_radius.name, FMAB.food_radius.array)
-# FMAB.spatial_distribution('markings')
+FMAB = ExperimentGroupBuilder(root).build(group)
+FMAB.load(['x0', 'y0', 'mm2px', 'entrance', 'crop_limit_x', 'food_center', 'ref_pts', 'entrance'])
+
+print(FMAB.x0.description, FMAB.x0.name, type(FMAB.x0.array.index[0][0]))
+print(FMAB.food_center.description, FMAB.food_center.name, type(FMAB.x0.array.index[0][0]))
+print(FMAB.crop_limit_x.description, FMAB.crop_limit_x.name)
+print(FMAB.mm2px.description, FMAB.mm2px.name)
+print(FMAB.ref_pts.description, FMAB.ref_pts.name)
+print(FMAB.entrance.description, FMAB.entrance.name)
 
 
