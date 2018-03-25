4b0392f52f084bcb08f9863d03835e097e4a720b
Line Le Goff <line.chloe.legoff@gmail.com>
2018-03-25 12:51:23 +0300

    type of Characteristics set as float)

--- CharacteristicsLoader.py
+++ CharacteristicsLoader.py
@@ -14,30 +14,30 @@ class CharacteristicsLoader:
 		if name.name in ['session', 'trial', 'n_frames', 'fps', 'mm2px', 'food_radius']:
 			res = pd.DataFrame(
 				[chara_dict[key][name.name] for key in chara_dict.keys()],
-				index=np.array(list(chara_dict.keys()), dtype=float),
+				index=np.array(list(chara_dict.keys()), dtype=int),
 				columns=[name.name])
 			res.index.name = 'id_exp'
 		elif name.name in ['food_center', 'traj_translation']:
 			res = pd.DataFrame(
-				[chara_dict[key][name.name] for key in chara_dict.keys()],
-				index=np.array(list(chara_dict.keys()), dtype=float),
+				[np.array(chara_dict[key][name.name], dtype=float) for key in chara_dict.keys()],
+				index=np.array(list(chara_dict.keys()), dtype=int),
 				columns=['x', 'y'])
 			res.index.name = 'id_exp'
 		elif name.name in ['crop_limit_x', 'crop_limit_y']:
 			res = pd.DataFrame(
-				[chara_dict[key][name.name] for key in chara_dict.keys()],
-				index=np.array(list(chara_dict.keys()), dtype=float),
+				[np.array(chara_dict[key][name.name], dtype=float) for key in chara_dict.keys()],
+				index=np.array(list(chara_dict.keys()), dtype=int),
 				columns=['l_min', 'l_max'])
 			res.index.name = 'id_exp'
 		elif name.name in ['ref_pts']:
 			res = pd.DataFrame(
-				[chara_dict[key][name.name][0]+chara_dict[key][name.name][1] for key in chara_dict.keys()],
-				index=np.array(list(chara_dict.keys()), dtype=float),
+				[np.array(chara_dict[key][name.name][0]+chara_dict[key][name.name][1], dtype=float) for key in chara_dict.keys()],
+				index=np.array(list(chara_dict.keys()), dtype=int),
 				columns=['x1', 'y1', 'x2', 'y2'])
 			res.index.name = 'id_exp'
 		elif name.name in ['entrance']:
 			res = pd.DataFrame(
-				[chara_dict[key][name.name][0]+chara_dict[key][name.name][1]+chara_dict[key][name.name][2]
+				[np.array(chara_dict[key][name.name][0]+chara_dict[key][name.name][1]+chara_dict[key][name.name][2], dtype=float)
 					for key in chara_dict.keys()],
 				index=np.array(list(chara_dict.keys()), dtype=int),
 				columns=['x1', 'y1', 'x2', 'y2', 'x3', 'y3'])
