4a7acd9fe23b5b575013b64d06b4ca31d000f038
Line Le Goff <line.chloe.legoff@gmail.com>
2018-03-25 20:49:25 +0300

    end of the day

--- ComputeTrajectory.py
+++ ComputeTrajectory.py
@@ -1,8 +1,8 @@
-import pandas as pd
+import numpy as np
 
+from Geometry import pts2vect, angle
 from ExperimentGroupBuilder import ExperimentGroupBuilder
-from Geometry import Geometry as Geo
-geo = Geo()
+from math import pi
 
 
 class ComputeTrajectory:
@@ -12,18 +12,31 @@ class ComputeTrajectory:
 	def centered_x_y(self, id_exp_list=None):
 		if id_exp_list is None:
 			id_exp_list = self.experiment.id_exp_list
-		self.experiment.load(['x0', 'y0', 'entrance', 'food_center', 'mm2px'])
-		x = self.experiment.x0.series - self.experiment.food_center.array['x']
-		y = self.experiment.y0.series - self.experiment.food_center.array['y']
-		x /= self.experiment.mm2px.array['mm2px']
-		y /= self.experiment.mm2px.array['mm2px']
+		self.experiment.load(['x0', 'y0', 'entrance1', 'entrance2', 'food_center', 'mm2px'])
+		self.experiment.x0.array['x0'] -= self.experiment.food_center.array['x']
 
-		for id_exp in id_exp_list:
-			entrance_pts1 = [self.experiment.entrance.array.loc[id_exp, 'x1'], self.experiment.entrance.array.loc[id_exp, 'y1']]
-			entrance_pts2 = [self.experiment.entrance.array.loc[id_exp, 'x2'], self.experiment.entrance.array.loc[id_exp, 'y2']]
-			u = Geo.pts2vect(entrance_pts1, entrance_pts2)
-			v = Geo.pts2vect(
-				[self.experiment.food_center.array.loc[id_exp, 'x'], self.experiment.food_center.array.loc[id_exp, 'y']],
-				Geo.segment_center(entrance_pts1, entrance_pts2))
-			a = geo.angle(u, v)
-			print(a)
+			# .operation(lambda z: z-self.experiment.food_center.array['x'])
+		self.experiment.y0.operation(lambda z: z-self.experiment.food_center.array['y'])
+
+		# self.experiment.x0.operation(lambda z: z-self.experiment.mm2px.array['mm2px'])
+		# self.experiment.y0.operation(lambda z: z-self.experiment.mm2px.array['mm2px'])
+		# for id_exp in id_exp_list:
+		# 	food_center = np.array(
+		# 		[self.experiment.food_center.array.loc[id_exp, 'x'], self.experiment.food_center.array.loc[id_exp, 'y']])
+		# 	entrance_pts1 = np.array(
+		# 		[self.experiment.entrance.array.loc[id_exp, 'x1'], self.experiment.entrance.array.loc[id_exp, 'y1']])
+		# 	entrance_pts2 = np.array(
+		# 		[self.experiment.entrance.array.loc[id_exp, 'x2'], self.experiment.entrance.array.loc[id_exp, 'y2']])
+		# 	u = pts2vect(entrance_pts1, entrance_pts2)
+		# 	a1 = angle([1, 0], u)
+		# 	entrance_pts1 -= food_center
+		# 	if abs(abs(a1)-0.5*pi) < abs(a1):
+		# 		a2 = angle([1, 0], entrance_pts1)
+		# 		if abs(abs(a2)-pi) < abs(a2):
+		# 			self.experiment.x0.series.loc[id_exp, :, :] *= -1
+		# 			self.experiment.y0.series.loc[id_exp, :, :] *= -1
+		# 	else:
+		# 		a2 = angle([0, 1], entrance_pts1)
+		# 		if abs(abs(a2)-pi) < abs(a2):
+		# 			self.experiment.x0.series.loc[id_exp, :, :] *= -1
+		# 			self.experiment.y0.series.loc[id_exp, :, :] *= -1
--- script_test.py
+++ script_test.py
@@ -11,30 +11,30 @@ group = 'FMAB'
 
 AnalyseStarter(root, group).start()
 
-NameBuild = NameBuilder(root, group)
-CharaLoad = CharacteristicsLoader(root, group)
-for name in ['food_radius', 'entrance1']:
-	Chara = CharaLoad.load(NameBuild.build(name))
-	print(Chara.array.head())
-	Chara.operation(lambda x: x/10.)
-	print(Chara.array.head())
-
-TSLoad = TimeSeriesLoader(root, group)
-for name in ['x0', 'major_axis_length']:
-	TS = TSLoad.load(NameBuild.build(name))
-	print(TS.array.head())
-	TS.operation(lambda x: x/10.)
-	print(TS.array.head())
-
-EventsLoad = EventsLoader(root, group)
-for name in ['markings']:
-	Ev = EventsLoad.load(NameBuild.build(name))
-	print(Ev.array.head())
-	Ev.operation(lambda x: x/10.)
-	print(Ev.array.head())
+# NameBuild = NameBuilder(root, group)
+# CharaLoad = CharacteristicsLoader(root, group)
+# for name in ['food_radius', 'entrance1']:
+# 	Chara = CharaLoad.load(NameBuild.build(name))
+# 	print(Chara.array.head())
+# 	Chara.operation(lambda x: x/10.)
+# 	print(Chara.array.head())
+#
+# TSLoad = TimeSeriesLoader(root, group)
+# for name in ['x0', 'major_axis_length']:
+# 	TS = TSLoad.load(NameBuild.build(name))
+# 	print(TS.array.head())
+# 	TS.operation(lambda x: x/10.)
+# 	print(TS.array.head())
+#
+# EventsLoad = EventsLoader(root, group)
+# for name in ['markings']:
+# 	Ev = EventsLoad.load(NameBuild.build(name))
+# 	print(Ev.array.head())
+# 	Ev.operation(lambda x: x/10.)
+# 	print(Ev.array.head())
 
 # FMAB = ExperimentGroupBuilder(root).build(group)
-# FMAB.load(['x0', 'y0', 'mm2px', 'entrance', 'crop_limit_x', 'food_center', 'ref_pts', 'entrance'])
+# FMAB.load(['x0', 'y0', 'mm2px', 'crop_limit_x', 'food_center', 'ref_pts2', 'entrance1'])
 # print(FMAB.x0.description, FMAB.x0.name, type(FMAB.x0.array.index[0][0]))
 # print(FMAB.food_center.description, FMAB.food_center.name, type(FMAB.x0.array.index[0][0]))
 # print(FMAB.crop_limit_x.description, FMAB.crop_limit_x.name)
@@ -42,5 +42,5 @@ for name in ['markings']:
 # print(FMAB.ref_pts.description, FMAB.ref_pts.name)
 # print(FMAB.entrance.description, FMAB.entrance.name)
 
-# traj = ComputeTrajectory(root, group)
-# traj.centered_x_y()
+traj = ComputeTrajectory(root, group)
+traj.centered_x_y()
