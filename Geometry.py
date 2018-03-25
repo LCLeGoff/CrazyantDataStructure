7799832b493273e10c7d686ffc80e904841217e7
Line Le Goff <line.chloe.legoff@gmail.com>
2018-03-25 12:14:18 +0300

    remove geometry class, files of functions instead

--- Geometry.py
+++ Geometry.py
@@ -1,25 +1,21 @@
 import numpy as np
 
 
-class Geometry:
-	def __init__(self):
-		pass
+def pts2vect(p, q):
+	return np.array(q)-np.array(p)
 
-	@staticmethod
-	def pts2vect(p, q):
-		return np.array(q)-np.array(p)
 
-	@staticmethod
-	def segment_center(p, q):
-		return (np.array(p)+np.array(q))/2.
+def segment_center(p, q):
+	return (np.array(p)+np.array(q))/2.
 
-	@staticmethod
-	def cross2d(u, v):
-		return u[0]*v[1]-u[1]*v[0]
 
-	@staticmethod
-	def dot2d(u, v):
-		return u[0]*v[0]+u[1]*v[1]
+def cross2d(u, v):
+	return u[0]*v[1]-u[1]*v[0]
 
-	def angle(self, u, v):
-		return np.arctan2(self.cross2d(u, v), self.dot2d(u, v))
+
+def dot2d(u, v):
+	return u[0]*v[0]+u[1]*v[1]
+
+
+def angle(u, v):
+	return np.arctan2(cross2d(u, v), dot2d(u, v))
