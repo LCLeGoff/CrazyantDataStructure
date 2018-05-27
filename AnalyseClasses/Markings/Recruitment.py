aedf05ffbc5f7fb722bb9dc329bbf507ccb36362
Line Le Goff <line.chloe.legoff@gmail.com>
2018-05-27 17:31:29 +0300

    Version final of marking batch interval computation

--- AnalyseClasses/Markings/Recruitment.py
+++ AnalyseClasses/Markings/Recruitment.py
@@ -44,8 +44,8 @@ class Recruitment:
 
 	def __compute_food_neighborhood_radius(self):
 		self.exp.add_copy1d('food_radius', 'food_neighborhood_radius')
-		food_neighborhood_radius_array = self.exp.food_radius.get_values() + 15
-		self.exp.food_neighborhood_radius.replace_values(food_neighborhood_radius_array)
+		food_neighborhood_radius_df = self.exp.food_radius.get_values() + 15
+		self.exp.food_neighborhood_radius.replace_values(food_neighborhood_radius_df)
 
 	def __convert_food_radius2mm(self):
 		fct_convert_in_mm = lambda x, y: round(x / y, 2)
@@ -77,13 +77,13 @@ class Recruitment:
 			t0 = 0
 		if t1 is None:
 			ax.plot(
-				self.exp.x.array.loc[exp, ant, t0:],
-				self.exp.y.array.loc[exp, ant, t0:],
+				self.exp.x.df.loc[exp, ant, t0:],
+				self.exp.y.df.loc[exp, ant, t0:],
 				c=c, ms=ms)
 		else:
 			ax.plot(
-				self.exp.x.array.loc[exp, ant, t0:t1],
-				self.exp.y.array.loc[exp, ant, t0:t1],
+				self.exp.x.df.loc[exp, ant, t0:t1],
+				self.exp.y.df.loc[exp, ant, t0:t1],
 				c=c, ms=ms)
 
 	def __plot_radial_zones(self, exp, ant, t):
@@ -103,24 +103,12 @@ class Recruitment:
 
 		self.__compute_xy_radial_zones()
 
-		dict_of_idx_exp_ant = self.exp.markings.get_id_exp_ant_dict()
-		array_of_idx_exp_ant = self.exp.markings.get_id_exp_ant_array()
+		dict_of_idx_exp_ant = self.exp.markings.get_dict_id_exp_ant()
+		array_of_idx_exp_ant = self.exp.markings.get_array_id_exp_ant()
 
-		self.exp.add_from_event_extraction(name='xy_radial_zones', new_name='zone_change_events')
+		self.exp.add_event_extracted_from_timeseries(name_ts='xy_radial_zones', name_extracted_events='zone_change_events')
 
-		self.exp.add_new1d_empty(
-			name='marking_batch_interval',
-			object_type='Events1d', category='Markings',
-			label='Marking batch interval',
-			description='Time intervals between the beginning and the end of the batches of the marking events'
-		)
-		self.exp.add_new1d_empty(
-			name='marking_batch_thresh',
-			object_type='Events1d', category='Markings',
-			label='Marking batch threshold',
-			description='Individual time threshold to define the batches of the marking events'
-		)
-		batch_list = []
+		batch_interval_list = []
 		batch_time_thresh_list = []
 		batch_distance_thresh_list = []
 		for id_exp in id_exp_list:
@@ -129,44 +117,124 @@ class Recruitment:
 				for id_ant in dict_of_idx_exp_ant[id_exp]:
 					if (id_exp, id_ant) in array_of_idx_exp_ant:
 
-						batch_time_thresh = self.__compute_batch_time_threshold(id_exp, id_ant)
-						batch_distance_thresh = self.__compute_batch_distance_threshold(id_exp, id_ant)
-						batch_time_thresh_list.append([id_exp, id_ant, batch_time_thresh])
-						batch_distance_thresh_list.append([id_exp, id_ant, batch_distance_thresh])
+						distance_thresh, time_thresh, batch_time_thresh_list, batch_distance_thresh_list = \
+							self.__compute_and_add_batch_thresholds(
+								id_exp, id_ant, batch_time_thresh_list, batch_distance_thresh_list)
 
-						ant_zone_change_event_array = self.__get_zone_change_events_for_id_ant(id_exp, id_ant)
-						mask_where_ant_enter_food_neighborhood = np.where(ant_zone_change_event_array[:, -1] == 2)[0]
-						nbr_entry_in_food_neighborhood = len(mask_where_ant_enter_food_neighborhood)
+						batch_interval_list = self.__add_ant_batches(id_exp, id_ant, time_thresh, distance_thresh, batch_interval_list)
 
-						for ii in range(nbr_entry_in_food_neighborhood-1):
-							idx_exit = mask_where_ant_enter_food_neighborhood[ii] + 1
-							idx_next_entry = mask_where_ant_enter_food_neighborhood[ii + 1]
-							list_visited_zones = list(ant_zone_change_event_array[idx_exit:idx_next_entry, -1])
-							has_ant_exit_circular_arena = (0 in list_visited_zones)
-							if has_ant_exit_circular_arena:
-								t_exit = ant_zone_change_event_array[idx_exit, 2]
-								t_next_entry = ant_zone_change_event_array[idx_next_entry, 2]
-								temp_batch_list = self.__compute_ant_batch(
-									id_exp, id_ant, batch_time_thresh, batch_distance_thresh, t_exit, t_next_entry)
-								batch_list.append(temp_batch_list)
-								self.__plot_batches(id_exp, id_ant, temp_batch_list, t_exit, t_next_entry)
+		self.__write_batch_intervals(batch_interval_list)
+		self.__write_batch_threshold(batch_time_thresh_list, batch_distance_thresh_list)
 
-						t_last_exit = ant_zone_change_event_array[mask_where_ant_enter_food_neighborhood[-1], 2]
-						temp_batch_list = self.__compute_ant_batch(
-							id_exp, id_ant, batch_time_thresh, batch_distance_thresh, t_last_exit)
-						batch_list.append(temp_batch_list)
-						self.__plot_batches(id_exp, id_ant, temp_batch_list, t_last_exit)
-			plt.show()
-		# 		for ii, batch in enumerate(batch_list):
-		# 			id_ant = int(batch[0, 1])
-		# 			t0 = int(batch[0, 2]) - 1
-		# 			dt = int(batch[-1, 2]) - t0 + 2
-		# 			self.exp.recruitment_interval.add_row((id_exp, id_ant, t0), dt)
-		# 			# self.__plot_recruitment_batch(id_exp, batch_list)
-		# 			# plt.show()
-		# self.exp.write('recruitment_interval')
+		# plt.show()
 
-	def __compute_ant_batch(self, id_exp, id_ant, time_thresh, distance_thresh, t0, t1=None):
+	def __write_batch_threshold(self, batch_time_thresh_list, batch_distance_thresh_list):
+		self.exp.add_new1d_from_array(
+			array=np.array(batch_time_thresh_list, dtype=int),
+			name='marking_batch_time_threshold',
+			object_type='AntCharacteristics1d', category='Markings',
+			label='Marking batch time threshold',
+			description='Individual time threshold to define the batches of the marking events'
+		)
+		self.exp.add_new1d_from_array(
+			array=np.array(batch_distance_thresh_list, dtype=int),
+			name='marking_batch_distance_threshold',
+			object_type='AntCharacteristics1d', category='Markings',
+			label='Marking batch distance threshold',
+			description='Individual distance threshold to define the batches of the marking events'
+		)
+		self.exp.write(['marking_batch_time_threshold', 'marking_batch_distance_threshold'])
+
+	def __write_batch_intervals(self, batch_interval_list):
+		self.exp.add_new1d_from_array(
+			array=np.array(batch_interval_list, dtype=int),
+			name='marking_batch_interval',
+			object_type='Events1d', category='Markings',
+			label='Marking batch interval',
+			description='Time intervals between the beginning and the end of the batches of the marking events'
+		)
+		self.exp.write(['marking_batch_interval'])
+
+	def __compute_and_add_batch_thresholds(self, id_exp, id_ant, batch_time_thresh_list, batch_distance_thresh_list):
+
+		time_thresh = self.__compute_batch_time_threshold(id_exp, id_ant)
+		distance_thresh = self.__compute_batch_distance_threshold(id_exp, id_ant)
+
+		batch_time_thresh_list.append([id_exp, id_ant, time_thresh])
+		batch_distance_thresh_list.append([id_exp, id_ant, distance_thresh])
+
+		return distance_thresh, time_thresh, batch_time_thresh_list, batch_distance_thresh_list
+
+	def __add_ant_batches(self, id_exp, id_ant, time_thresh, distance_thresh, batch_list):
+
+		ant_zone_change_event_array = self.__get_zone_change_events_for_id_ant(id_exp, id_ant)
+		idx_when_ant_enters_food_neighborhood = np.where(ant_zone_change_event_array[:, -1] == 2)[0]
+
+		batch_list = self.__add_batches_during_loops_around_food(
+			id_exp, id_ant, time_thresh, distance_thresh,
+			idx_when_ant_enters_food_neighborhood,
+			ant_zone_change_event_array, batch_list)
+
+		batch_list = self.__add_batches_between_last_food_entry_and_experiment_end(
+			id_exp, id_ant, time_thresh, distance_thresh,
+			idx_when_ant_enters_food_neighborhood,
+			ant_zone_change_event_array, batch_list)
+
+		return batch_list
+
+	def __add_batches_between_last_food_entry_and_experiment_end(
+			self, id_exp, id_ant, time_thresh, distance_thresh,
+			idx_when_ant_enters_food_neighborhood, ant_zone_change_event_array, batch_interval_list):
+
+		t_last_exit = ant_zone_change_event_array[idx_when_ant_enters_food_neighborhood[-1], 2]
+
+		batch_list = self.__compute_ant_batches(
+			id_exp, id_ant, time_thresh, distance_thresh, t_last_exit)
+
+		batch_interval_list = self.__add_batch_intervals(batch_list, batch_interval_list)
+
+		# self.__plot_batches(id_exp, id_ant, batch_list, t_last_exit)
+
+		return batch_interval_list
+
+	def __add_batches_during_loops_around_food(
+			self, id_exp, id_ant, time_thresh, distance_thresh,
+			idx_when_ant_enters_food_neighborhood, ant_zone_change_event_array, batch_interval_list):
+
+		nbr_entry_in_food_neighborhood = len(idx_when_ant_enters_food_neighborhood)
+
+		for ii in range(nbr_entry_in_food_neighborhood - 1):
+
+			idx_exit = idx_when_ant_enters_food_neighborhood[ii] + 1
+			idx_next_entry = idx_when_ant_enters_food_neighborhood[ii + 1]
+
+			list_visited_zones = list(ant_zone_change_event_array[idx_exit:idx_next_entry, -1])
+			has_ant_exit_circular_arena = (0 in list_visited_zones)
+
+			if has_ant_exit_circular_arena:
+				t_exit = ant_zone_change_event_array[idx_exit, 2]
+				t_next_entry = ant_zone_change_event_array[idx_next_entry, 2]
+
+				batch_list = self.__compute_ant_batches(
+					id_exp, id_ant, time_thresh, distance_thresh, t_exit, t_next_entry)
+
+				batch_interval_list = self.__add_batch_intervals(batch_list, batch_interval_list)
+
+				# self.__plot_batches(id_exp, id_ant, batch_list, t_exit, t_next_entry)
+
+		return batch_interval_list
+
+	@staticmethod
+	def __add_batch_intervals(batch_list, batch_interval_list):
+		for batch in batch_list:
+			id_exp = batch[0][0]
+			id_ant = batch[0][1]
+			frame = batch[0][2] - 1
+			dt = batch[-1][2] - frame + 1
+			batch_interval_list.append([id_exp, id_ant, frame, dt])
+		return batch_interval_list
+
+	def __compute_ant_batches(self, id_exp, id_ant, time_thresh, distance_thresh, t0, t1=None):
 		xy_mark_array = self.__get_xy_array_of_marking_between_t0_t1(id_exp, id_ant, t0, t1)
 		is_there_marking_between_t0_t1 = len(xy_mark_array) != 0
 		if is_there_marking_between_t0_t1:
@@ -175,29 +243,6 @@ class Recruitment:
 			return batch_list
 		else:
 			return []
-
-	def __correct_batches_based_on_distance_threshold(self, batch_list, max_batch_distance):
-		batch_list2 = []
-		for batch in batch_list:
-			batch_xy = np.array(batch)[:, -2:]
-			batch_xy_distance = np.around(distance(batch_xy[1:, :], batch_xy[:-1, :]))
-			mask_where_distance_to_long = np.where(batch_xy_distance > max_batch_distance)[0]
-			is_there_distance_to_long = len(mask_where_distance_to_long) != 0
-			if is_there_distance_to_long:
-				kk1 = mask_where_distance_to_long[0] + 1
-				if kk1 > 3:
-					batch_list2.append(batch[:kk1])
-				for jj in range(1, len(mask_where_distance_to_long)):
-					kk0 = mask_where_distance_to_long[jj - 1] + 1
-					kk1 = mask_where_distance_to_long[jj] + 1
-					if kk1 - kk0 > 2:
-						batch_list2.append(batch[kk0: kk1])
-				if len(batch_xy) - mask_where_distance_to_long[-1] > 3:
-					kk0 = mask_where_distance_to_long[-1] + 1
-					batch_list2.append(batch[kk0:])
-			else:
-				batch_list2.append(batch)
-		return batch_list2
 
 	def __compute_batches_based_on_time_threshold(self, thresh, xy_mark_array):
 		batch_list = []
@@ -211,6 +256,54 @@ class Recruitment:
 				current_batch = self.__start_new_batch(jj, xy_mark_array)
 		batch_list = self.__add_current_batch_in_batch_list(batch_list, current_batch)
 		return batch_list
+
+	def __correct_batches_based_on_distance_threshold(self, batch_list, max_batch_distance):
+		batch_list2 = []
+		for batch in batch_list:
+			idx_when_distance_to_big = self.__find_idx_when_distance_is_to_big(batch, max_batch_distance)
+			is_there_distance_to_big = len(idx_when_distance_to_big) != 0
+			if is_there_distance_to_big:
+				batch_list2 = self.__add_first_sub_batch(batch, batch_list2, idx_when_distance_to_big)
+				batch_list2 = self.__add_middle_sub_batches(batch, batch_list2, idx_when_distance_to_big)
+				batch_list2 = self.__add_last_sub_batch(batch, batch_list2, idx_when_distance_to_big)
+			else:
+				batch_list2.append(batch)
+		return batch_list2
+
+	@staticmethod
+	def __add_last_sub_batch(batch, batch_list2, idx_when_distance_to_big):
+		first_idx_last_sub_batch = idx_when_distance_to_big[-1] + 1
+		size_last_sub_batch = len(batch) - first_idx_last_sub_batch
+		if_last_sub_batch_big_enough = size_last_sub_batch > 2
+		if if_last_sub_batch_big_enough:
+			batch_list2.append(batch[first_idx_last_sub_batch:])
+		return batch_list2
+
+	@staticmethod
+	def __add_middle_sub_batches(batch, batch_list2, idx_when_distance_to_big):
+		for jj in range(1, len(idx_when_distance_to_big)):
+			first_idx_sub_batch = idx_when_distance_to_big[jj - 1] + 1
+			last_idx_sub_batch = idx_when_distance_to_big[jj] + 1
+			size_sub_batch = last_idx_sub_batch - first_idx_sub_batch
+			if_sub_batch_big_enough = size_sub_batch > 2
+			if if_sub_batch_big_enough > 2:
+				batch_list2.append(batch[first_idx_sub_batch: last_idx_sub_batch])
+		return batch_list2
+
+	@staticmethod
+	def __add_first_sub_batch(batch, batch_list2, idx_when_distance_to_big):
+		size_first_sub_batch = idx_when_distance_to_big[0]
+		if_first_sub_batch_big_enough = size_first_sub_batch > 2
+		if if_first_sub_batch_big_enough > 3:
+			batch_list2.append(batch[:size_first_sub_batch + 1])
+		return batch_list2
+
+	@staticmethod
+	def __find_idx_when_distance_is_to_big(batch, max_batch_distance):
+		batch_xy = np.array(batch)[:, -2:]
+		batch_xy_distance = np.around(distance(batch_xy[1:, :], batch_xy[:-1, :]))
+		idx_when_distance_to_big = np.where(batch_xy_distance > max_batch_distance)[0]
+		return idx_when_distance_to_big
 
 	@staticmethod
 	def __start_new_batch(jj, xy_mark_array):
@@ -239,10 +332,10 @@ class Recruitment:
 
 		self.__compute_xy_radial_zones()
 
-		dict_of_idx_exp_ant = self.exp.markings.get_id_exp_ant_dict()
-		array_of_idx_exp_ant = self.exp.markings.get_id_exp_ant_array()
+		dict_of_idx_exp_ant = self.exp.markings.get_dict_id_exp_ant()
+		array_of_idx_exp_ant = self.exp.markings.get_array_id_exp_ant()
 
-		self.exp.add_from_event_extraction(name='xy_radial_zones', new_name='zone_change_events')
+		self.exp.add_event_extracted_from_timeseries(name_ts='xy_radial_zones', name_extracted_events='zone_change_events')
 
 		self.exp.add_new1d_empty(
 			name='recruitment_interval',
@@ -303,13 +396,13 @@ class Recruitment:
 			t0 = 0
 		if t1 is None:
 			ax.plot(
-				self.exp.xy_markings.array.loc[IdxSc[id_exp, id_ant, t0:], 'x'],
-				self.exp.xy_markings.array.loc[IdxSc[id_exp, id_ant, t0:], 'y'],
+				self.exp.xy_markings.df.loc[IdxSc[id_exp, id_ant, t0:], 'x'],
+				self.exp.xy_markings.df.loc[IdxSc[id_exp, id_ant, t0:], 'y'],
 				'o', ms=5, c=c)
 		else:
 			ax.plot(
-				self.exp.xy_markings.array.loc[IdxSc[id_exp, id_ant, t0:t1], 'x'],
-				self.exp.xy_markings.array.loc[IdxSc[id_exp, id_ant, t0:t1], 'y'],
+				self.exp.xy_markings.df.loc[IdxSc[id_exp, id_ant, t0:t1], 'x'],
+				self.exp.xy_markings.df.loc[IdxSc[id_exp, id_ant, t0:t1], 'y'],
 				'o', ms=5, c=c)
 
 	def __get_zone_change_events_for_id_ant(self, id_exp, id_ant):
@@ -319,7 +412,7 @@ class Recruitment:
 
 	def __compute_batch_time_threshold(self, id_exp, id_ant):
 		self.exp.load('marking_interval')
-		mark_interval_ant = np.array(self.exp.marking_interval.array.loc[id_exp, id_ant, :].reset_index())
+		mark_interval_ant = np.array(self.exp.marking_interval.df.loc[id_exp, id_ant, :].reset_index())
 		n_occ, times = np.histogram(mark_interval_ant[:, -1], bins='fd')
 		min_thresh = 60
 		max_thresh = 200
@@ -330,12 +423,12 @@ class Recruitment:
 
 	def __compute_batch_distance_threshold(self, id_exp, id_ant):
 		self.exp.load('marking_distance')
-		mark_distance_ant = np.array(self.exp.marking_distance.array.loc[id_exp, id_ant, :].reset_index())
+		mark_distance_ant = np.array(self.exp.marking_distance.df.loc[id_exp, id_ant, :].reset_index())
 		n_occ, times = np.histogram(mark_distance_ant[:, -1], bins='fd')
 		min_thresh = 40
 		max_thresh = 70
 		thresh = self.__compute_batch_threshold(n_occ, times, min_thresh, max_thresh)
-		self.__plot_batch_thresh(id_exp, id_ant, max_thresh, min_thresh, n_occ, thresh, times, title='distance')
+		# self.__plot_batch_thresh(id_exp, id_ant, max_thresh, min_thresh, n_occ, thresh, times, title='distance')
 
 		return thresh
 
@@ -357,7 +450,7 @@ class Recruitment:
 				thresh = np.floor(times3[-1])
 			else:
 				thresh = np.floor(times3[np.where(n_occ2 == 0)[0][0]])
-		thresh = max(min(thresh, max_thresh), min_thresh)
+		thresh = int(max(min(thresh, max_thresh), min_thresh))
 		return thresh
 
 	@staticmethod
