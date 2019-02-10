2c7e7a7bbf5021ed289f902283873fc2fe54c0fa
Unknown <line.chloe.legoff@gmail.com>
2019-02-10 18:15:31 +0200

    Movie class to prepare dynamical check

---
+++ Movies/Movies.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+class Movies:
+    def __init__(self, add):
+        self.add = add
+        self.cap = cv2.VideoCapture(self.add)
+
+    def display(self):
+
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            cv2.imshow('frame', gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def get_frame(self, frame):
+
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
+        ret, frame_img = self.cap.read()
+        frame_img = cv2.cvtColor(frame_img, cv2.COLOR_BGR2GRAY)
+        cv2.destroyAllWindows()
+        return frame_img
