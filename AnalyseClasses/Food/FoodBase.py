e7f862d6b9148471d53b5c08e6df3278c3282215
Unknown <line.chloe.legoff@gmail.com>
2019-03-06 18:33:12 +0200

    refactoring

--- AnalyseClasses/Food/FoodBase.py
+++ AnalyseClasses/Food/FoodBase.py
@@ -3,11 +3,12 @@ import pandas as pd
 from sklearn import svm
 
 from DataStructure.Builders.ExperimentGroupBuilder import ExperimentGroupBuilder
+from ExperimentGroups import ExperimentGroups
 from Tools.MiscellaneousTools.Geometry import angle_df, norm_angle_tab, norm_angle_tab2
 
 
 class AnalyseFoodBase:
-    def __init__(self, root, group, exp=None):
+    def __init__(self, root, group, exp: ExperimentGroups = None):
         if exp is None:
             self.exp = ExperimentGroupBuilder(root).build(group)
         else:
@@ -529,61 +530,60 @@ class AnalyseFoodBase:
         self.exp.write(res_name)
 
     def compute_is_carrying(self):
-        name_result = 'is_carrying'
+        name_result = 'carrying_next2food'
 
         speed_name = 'mm20_speed_next2food'
         orientation_name = 'mm20_angle_body_food_next2food'
         distance_name = 'mm20_distance2food_next2food'
         distance_diff_name = 'mm20_distance2food_next2food_diff'
-        food_radius_name = 'food_radius'
         training_set_name = 'carrying_training_set'
 
         self.exp.load(
-            [training_set_name, speed_name, orientation_name,
-             distance_name, distance_diff_name, food_radius_name, 'mm2px'])
+            [training_set_name, speed_name, orientation_name, distance_name, distance_diff_name, 'mm2px'])
 
         self.exp.get_data_object(orientation_name).change_values(self.exp.get_df(orientation_name).abs())
-        food_radius_df = self.exp.get_reindexed_df(name_to_reindex=food_radius_name, reindexer_name=orientation_name)
 
         df_features, df_labels = self.__get_training_features_and_labels4carrying(
-            speed_name, orientation_name, distance_name, distance_diff_name, food_radius_df, training_set_name)
+            speed_name, orientation_name, distance_name, distance_diff_name, training_set_name)
 
         df_to_predict = self.__get_df_to_predict_carrying(
-            distance_diff_name, distance_name, food_radius_df, orientation_name, speed_name)
+            distance_diff_name, distance_name, orientation_name, speed_name)
 
         clf = svm.SVC(kernel='rbf', gamma='auto')
         clf.fit(df_features.loc[pd.IndexSlice[1, :, :], :], df_labels.loc[pd.IndexSlice[1, :, :], :])
-        prediction = clf.predict(df_to_predict.loc[pd.IndexSlice[1, :, :], :])
+        prediction1 = clf.predict(df_to_predict.loc[pd.IndexSlice[1, :, :], :])
 
         clf = svm.SVC(kernel='rbf', gamma='auto')
         clf.fit(df_features.loc[pd.IndexSlice[2:, :, :], :], df_labels.loc[pd.IndexSlice[2:, :, :], :])
         prediction2 = clf.predict(df_to_predict.loc[pd.IndexSlice[2:, :, :], :])
-        prediction = np.concatenate(prediction, prediction2)
+
+        prediction = np.zeros(len(prediction1)+len(prediction2), dtype=int)
+        prediction[:len(prediction1)] = prediction1
+        prediction[len(prediction1):] = prediction2
 
         self.exp.add_copy(
-            old_name=speed_name, new_name=name_result,
-            category='FoodBase', label='Is ant carrying?',
-            description='Boolean giving if ants are carrying or not'
+            old_name=distance_name, new_name=name_result,
+            category='FoodCarrying', label='Is ant carrying?',
+            description='Boolean giving if ants are carrying or not, for the ants next to the food'
         )
         self.exp.__dict__[name_result].df = self.exp.get_df(name_result).reindex(df_to_predict.index)
         self.exp.get_data_object(name_result).change_values(prediction)
         self.exp.write(name_result)
 
-    def __get_df_to_predict_carrying(self, distance_diff_name, distance_name, food_radius_df,
-                                     orientation_name, speed_name):
-        df_to_predict = self.exp.get_df(speed_name).join(self.exp.get_df(orientation_name), how='inner')
+    def __get_df_to_predict_carrying(
+            self, distance_diff_name, distance_name, orientation_name, speed_name):
+
+        df_to_predict = self.exp.get_df(distance_name).join(self.exp.get_df(orientation_name), how='inner')
         self.exp.remove_object(orientation_name)
-        df_to_predict = df_to_predict.join(self.exp.get_df(distance_name), how='inner')
-        self.exp.remove_object(distance_name)
+        df_to_predict = df_to_predict.join(self.exp.get_df(speed_name), how='inner')
+        self.exp.remove_object(speed_name)
         df_to_predict = df_to_predict.join(self.exp.get_df(distance_diff_name), how='inner')
         self.exp.remove_object(distance_diff_name)
-        # df_to_predict = df_to_predict.join(food_radius_df, how='inner')
-        # self.exp.remove_object(food_radius_name)
         df_to_predict.dropna(inplace=True)
         return df_to_predict
 
     def __get_training_features_and_labels4carrying(
-            self, speed_name, orientation_name, distance_name, distance_diff_name, food_radius_df, training_set_name):
+            self, speed_name, orientation_name, distance_name, distance_diff_name, training_set_name):
 
         self.exp.filter_with_time_occurrences(
             name_to_filter=speed_name, filter_name=training_set_name,
@@ -601,10 +601,9 @@ class AnalyseFoodBase:
             name_to_filter=distance_diff_name, filter_name=training_set_name,
             result_name='training_set_distance_diff', replace=True)
 
-        df_features = self.exp.training_set_speed.df.join(self.exp.training_set_orientation.df, how='inner')
-        df_features = df_features.join(self.exp.training_set_distance.df, how='inner')
+        df_features = self.exp.training_set_distance.df.join(self.exp.training_set_orientation.df, how='inner')
+        df_features = df_features.join(self.exp.training_set_speed.df, how='inner')
         df_features = df_features.join(self.exp.training_set_distance_diff.df, how='inner')
-        # df_features = df_features.join(food_radius_df, how='inner')
         df_features.dropna(inplace=True)
 
         self.exp.remove_object('training_set_speed')
