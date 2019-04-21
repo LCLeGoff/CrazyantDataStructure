1dda8139d3eff842f802d0d39f718d0b30d3a1c2
Line Le Goff <line.chloe.legoff@gmail.com>
2019-04-21 19:13:36 +0300

    add address to movies when at home

--- Scripts/root.py
+++ Scripts/root.py
@@ -3,5 +3,5 @@
 # root = '/Data/Dropbox/POSTDOC/CrazyAnt/Results_python/Data/'
 root = '/home/line/Dropbox/POSTDOC/CrazyAnt/Results_python/Data/'
 
-root_movie = '/data/POSTDOC/CrazyAnt/'
-
+# root_movie = '/data/POSTDOC/CrazyAnt/'
+root_movie = '/media/line/Dell Portable Hard Drive/Crazy_ant/'
