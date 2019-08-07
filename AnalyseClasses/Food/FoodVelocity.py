Fixing food velocity phi computation

AnalyseClasses/Food/FoodVelocity.py
@@ -86,6 +86,7 @@ class AnalyseFoodVelocity(AnalyseClassDecorator):
     def compute_food_velocity_phi_evol(self, redo=False):
         name = 'food_velocity_phi'
         result_name = name+'_hist_evol'
+        init_frame_name = 'food_first_frame'
 
         dtheta = np.pi/25.
         bins = np.arange(0, np.pi+dtheta, dtheta)
@@ -95,7 +96,18 @@ class AnalyseFoodVelocity(AnalyseClassDecorator):
         end_frame_intervals = start_frame_intervals + dx*60*100
 
         if redo:
-            self.exp.load(name)
+            self.exp.load([name, init_frame_name])
+
+            new_times = 'new_times'
+            self.exp.add_copy1d(name_to_copy=name, copy_name=new_times, replace=True)
+            self.exp.get_df(new_times).loc[:, new_times] = self.exp.get_index(new_times).get_level_values(id_frame_name)
+            self.exp.operation_between_2names(name1=new_times, name2=init_frame_name, func=lambda x, y: x - y)
+            self.exp.get_df(new_times).reset_index(inplace=True)
+
+            self.exp.get_df(name).reset_index(inplace=True)
+            self.exp.get_df(name).loc[:, id_frame_name] = self.exp.get_df(new_times).loc[:, new_times]
+            self.exp.get_df(name).set_index([id_exp_name, id_frame_name], inplace=True)
+
             self.exp.operation(name, lambda x: np.abs(x))
             self.exp.hist1d_evolution(name_to_hist=name, start_index_intervals=start_frame_intervals,
                                       end_index_intervals=end_frame_intervals, bins=bins,